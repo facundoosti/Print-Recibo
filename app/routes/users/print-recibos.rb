7029f94a8213b7933f05b12e333a2cf9c281f00e
facundoosti <facundoosti@gmail.com>
2017-02-21 21:25:16 -0300

    t

--- app/routes/users/print-recibos.rb
+++ app/routes/users/print-recibos.rb
@@ -6,8 +6,17 @@ module PrintRecibos
     post '/print-recibos' do
 
       begin
+        filename = params[:file][:filename]
+        file = params[:file][:tempfile]
+
+        File.open("./file/#{filename}", 'wb') do |f|
+          f.write(file.read)
+        end
+
         registers = read(params['file']) 
         
+        File.delete("./file/#{filename}")
+
         info = {
             consorcio: Consorcios.find(params['consorcio_id']),
             periodo: mes(Date::strptime(params['month'], '%Y-%m').month).capitalize,
